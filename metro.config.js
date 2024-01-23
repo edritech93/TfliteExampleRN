87a9ab79aa16fdf0098c2d685e4dff30fba27498
Yudi Alviska <yudi.a@weefer.co.id>
2024-01-23 13:17:13 +0700

    add code example

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +6,45 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+// const config = {
+//   // assetExts: ['tflite'],
+// };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// module.exports = (async () => {
+//   const defaultConfig = await getDefaultConfig();
+//   const {assetExts} = defaultConfig.resolver;
+//   return {
+//     resolver: {
+//       // Add bin to assetExts
+//       assetExts: [...assetExts, 'tflite'],
+//     },
+//   };
+// })();
+
+const path = require('path');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+const {
+  resolver: {sourceExts, assetExts},
+} = defaultConfig;
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  resolver: {
+    assetExts: [...assetExts, 'tflite'],
+    sourceExts: [...sourceExts, 'tflite'],
+    resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
+  },
+  watchFolders: [path.resolve(__dirname, '../')],
+};
+
+module.exports = mergeConfig(defaultConfig, config);
