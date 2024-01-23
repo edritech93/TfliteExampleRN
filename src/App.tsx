e1f30c7af461824cff21429e6bdbb4d540edc1a2
Yudi Alviska <yudi.a@weefer.co.id>
2024-01-23 13:19:48 +0700

    edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,112 +1,112 @@
-// import * as React from 'react';
-// import {StyleSheet, View, Text, ActivityIndicator} from 'react-native';
-// import {
-//   Tensor,
-//   TensorflowModel,
-//   useTensorflowModel,
-// } from 'react-native-fast-tflite';
-// import {
-//   Camera,
-//   useCameraDevice,
-//   useCameraPermission,
-//   useFrameProcessor,
-// } from 'react-native-vision-camera';
-// import {useResizePlugin} from 'vision-camera-resize-plugin';
+import * as React from 'react';
+import {StyleSheet, View, Text, ActivityIndicator} from 'react-native';
+import {
+  Tensor,
+  TensorflowModel,
+  useTensorflowModel,
+} from 'react-native-fast-tflite';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
+import {useResizePlugin} from 'vision-camera-resize-plugin';
 
-// function tensorToString(tensor: Tensor): string {
-//   return `\n  - ${tensor.dataType} ${tensor.name}[${tensor.shape}]`;
-// }
-// function modelToString(model: TensorflowModel): string {
-//   return (
-//     `TFLite Model (${model.delegate}):\n` +
-//     `- Inputs: ${model.inputs.map(tensorToString).join('')}\n` +
-//     `- Outputs: ${model.outputs.map(tensorToString).join('')}`
-//   );
-// }
-
-// export default function App(): React.ReactNode {
-//   const {hasPermission, requestPermission} = useCameraPermission();
-//   const device = useCameraDevice('back');
-
-//   const model = useTensorflowModel(require('./assets/object_detector.tflite'));
-//   const actualModel = model.state === 'loaded' ? model.model : undefined;
-
-//   React.useEffect(() => {
-//     if (actualModel == null) return;
-//     console.log(`Model loaded! Shape:\n${modelToString(actualModel)}]`);
-//   }, [actualModel]);
-
-//   const {resize} = useResizePlugin();
-
-//   const frameProcessor = useFrameProcessor(
-//     frame => {
-//       'worklet';
-//       if (actualModel == null) {
-//         // model is still loading...
-//         return;
-//       }
-
-//       console.log(`Running inference on ${frame}`);
-//       const resized = resize(frame, {
-//         size: {
-//           width: 640,
-//           height: 640,
-//         },
-//         pixelFormat: 'rgb-uint8',
-//       });
-//       const result = actualModel.runSync([resized]);
-//       console.log('Result: ' + result.length);
-//     },
-//     [actualModel],
-//   );
-
-//   React.useEffect(() => {
-//     requestPermission();
-//   }, [requestPermission]);
-
-//   console.log(`Model: ${model.state} (${model.model != null})`);
-
-//   return (
-//     <View style={styles.container}>
-//       {hasPermission && device != null ? (
-//         <Camera
-//           device={device}
-//           style={StyleSheet.absoluteFill}
-//           isActive={true}
-//           frameProcessor={frameProcessor}
-//         />
-//       ) : (
-//         <Text>No Camera available.</Text>
-//       )}
-
-//       {model.state === 'loading' && (
-//         <ActivityIndicator size="small" color="white" />
-//       )}
-
-//       {model.state === 'error' && (
-//         <Text>Failed to load model! {model.error.message}</Text>
-//       )}
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-
-export default function App() {
+function tensorToString(tensor: Tensor): string {
+  return `\n  - ${tensor.dataType} ${tensor.name}[${tensor.shape}]`;
+}
+function modelToString(model: TensorflowModel): string {
   return (
-    <View>
-      <Text>App</Text>
+    `TFLite Model (${model.delegate}):\n` +
+    `- Inputs: ${model.inputs.map(tensorToString).join('')}\n` +
+    `- Outputs: ${model.outputs.map(tensorToString).join('')}`
+  );
+}
+
+export default function App(): React.ReactNode {
+  const {hasPermission, requestPermission} = useCameraPermission();
+  const device = useCameraDevice('back');
+
+  const model = useTensorflowModel(require('./assets/object_detector.tflite'));
+  const actualModel = model.state === 'loaded' ? model.model : undefined;
+
+  React.useEffect(() => {
+    if (actualModel == null) return;
+    console.log(`Model loaded! Shape:\n${modelToString(actualModel)}]`);
+  }, [actualModel]);
+
+  const {resize} = useResizePlugin();
+
+  const frameProcessor = useFrameProcessor(
+    frame => {
+      'worklet';
+      if (actualModel == null) {
+        // model is still loading...
+        return;
+      }
+
+      console.log(`Running inference on ${frame}`);
+      const resized = resize(frame, {
+        size: {
+          width: 640,
+          height: 640,
+        },
+        pixelFormat: 'rgb-uint8',
+      });
+      const result = actualModel.runSync([resized]);
+      console.log('Result: ' + result.length);
+    },
+    [actualModel],
+  );
+
+  React.useEffect(() => {
+    requestPermission();
+  }, [requestPermission]);
+
+  console.log(`Model: ${model.state} (${model.model != null})`);
+
+  return (
+    <View style={styles.container}>
+      {hasPermission && device != null ? (
+        <Camera
+          device={device}
+          style={StyleSheet.absoluteFill}
+          isActive={true}
+          frameProcessor={frameProcessor}
+        />
+      ) : (
+        <Text>No Camera available.</Text>
+      )}
+
+      {model.state === 'loading' && (
+        <ActivityIndicator size="small" color="white" />
+      )}
+
+      {model.state === 'error' && (
+        <Text>Failed to load model! {model.error.message}</Text>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+// import {StyleSheet, Text, View} from 'react-native';
+// import React from 'react';
+
+// export default function App() {
+//   return (
+//     <View>
+//       <Text>App</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({});
