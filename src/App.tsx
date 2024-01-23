3786672b73917547e1d633f1848c04453f3c8251
Yudi Alviska <yudi.a@weefer.co.id>
2024-01-23 14:35:05 +0700

    edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {
   Camera,
@@ -8,19 +8,35 @@ import {
   useFrameProcessor,
 } from 'react-native-vision-camera';
 import {useResizePlugin} from 'vision-camera-resize-plugin';
-import {useTensorflowModel} from 'react-native-fast-tflite';
+import {
+  TensorflowModel,
+  loadTensorflowModel,
+  useTensorflowModel,
+} from 'react-native-fast-tflite';
 
 export default function App(): React.ReactNode {
   const {hasPermission, requestPermission} = useCameraPermission();
   const {resize} = useResizePlugin();
-  const {model} = useTensorflowModel(
-    require('./assets/object_detector.tflite'),
-  );
+  // const {model} = useTensorflowModel(
+  //   require('./assets/object_detector.tflite'),
+  // );
   const device = useCameraDevice('back');
+
+  const [model, setModel] = useState<TensorflowModel | null>(null);
 
   useEffect(() => {
     requestPermission();
   }, [requestPermission]);
+
+  useEffect(() => {
+    async function _loadModel() {
+      const objModel: TensorflowModel = await loadTensorflowModel(
+        require('./assets/object_detector.tflite'),
+      );
+      setModel(objModel);
+    }
+    _loadModel();
+  }, []);
 
   const frameProcessor = useFrameProcessor(
     (frame: Frame) => {
@@ -36,8 +52,30 @@ export default function App(): React.ReactNode {
         pixelFormat: 'rgb-uint8',
       });
       const array = new Uint8Array(data);
-      const output = model.runSync([array]);
-      console.log('Result: ' + output.length);
+      // const output = model.runSync([data]);
+      const output = model.runSync([array] as any[]);
+      // console.log('Result: ' + output.length);
+
+      const detection_boxes = output[0];
+      const detection_classes = output[1];
+      const detection_scores = output[2];
+      const num_detections = output[3];
+      // console.log(`Detected ${num_detections} objects!`);
+
+      for (let i = 0; i < detection_boxes.length; i += 4) {
+        const confidence = detection_scores[i / 4];
+        console.log('confidence => ', confidence);
+        // npm install @shopify/react-native-skia
+        // if (confidence > 0.7) {
+        //   // 4. Draw a red box around the detected object!
+        //   const left = detection_boxes[i];
+        //   const top = detection_boxes[i + 1];
+        //   const right = detection_boxes[i + 2];
+        //   const bottom = detection_boxes[i + 3];
+        //   const rect = SkRect.Make(left, top, right, bottom);
+        //   frame.drawRect(rect, SkColors.Red);
+        // }
+      }
     },
     [model],
   );
